92c77161c4d042b8eccf56faf419c1d040860594
masterUNG <master@abc.com>
2020-02-08 01:05:00 -0800

    Update

--- lib/widget/main_home.dart
+++ lib/widget/main_home.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:ungshoppee/utility/my_constant.dart';
 import 'package:dio/dio.dart';
@@ -11,32 +12,88 @@ class MainHome extends StatefulWidget {
 
 class _MainHomeState extends State<MainHome> {
   // Field
+  List<Widget> listBanner = List();
 
   // Method
- @override
- void initState() { 
-   super.initState();
-   readBanner();
- }
+  @override
+  void initState() {
+    super.initState();
+    readBanner();
+  }
 
- Future<void> readBanner()async{
+  Widget group1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Icon(Icons.android),
+          Text('Group 1'),
+        ],
+      ),
+    );
+  }
 
-   Response response = await Dio().get(MyConstant().urlBanner);
-   print('response = $response');
+  Widget row1Group() {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        group1(),
+        group1(),
+        group1(),
+      ],
+    );
+  }
 
- }
+  Widget groupProduct() {
+    return Column(
+      children: <Widget>[
+        row1Group(),
+        row1Group(),
+      ],
+    );
+  }
 
+  Future<void> readBanner() async {
+    Response response = await Dio().get(MyConstant().urlBanner);
+    // print('response = $response');
+
+    var result = json.decode(response.data);
+
+    for (var map in result) {
+      Widget myWidget = Container(
+        child: Image.network(map['Url']),
+      );
+      setState(() {
+        listBanner.add(myWidget);
+      });
+    }
+  }
 
   Widget banner() {
-    return Container(color: Colors.green,
+    return Container(
+      // color: Colors.green,
       height: 200.0,
+      child: listBanner.length == 0
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : CarouselSlider(
+              items: listBanner,
+              aspectRatio: 16 / 9,
+              enlargeCenterPage: true,
+              pauseAutoPlayOnTouch: Duration(seconds: 3),
+              autoPlay: true,
+              autoPlayAnimationDuration: Duration(seconds: 3),
+            ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: <Widget>[banner(),],
+      children: <Widget>[
+        banner(),
+        groupProduct(),
+      ],
     );
   }
 }

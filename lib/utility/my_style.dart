9cbdb35e94d0cd1507fd3577ab136a1bb95f8dbb
masterUNG <master@abc.com>
2020-02-08 23:28:03 -0800

    Detail Success

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -1,10 +1,18 @@
-class MyStyle {
+import 'package:flutter/material.dart';
 
+class MyStyle {
   String appName = 'Ung ShoppEE';
 
+  TextStyle headTextStyle =
+      TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold);
+
+ TextStyle headColorTextStyle =
+      TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.red);
 
 
+  TextStyle bodyTextStyle = TextStyle(
+    fontSize: 18.0,
+  );
 
   MyStyle();
-  
 }
